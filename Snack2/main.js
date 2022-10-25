9b67923253228c651b396a09c57c6fc4d0676876
MarcoPerta <perta.marco@gmail.com>
2022-10-25 14:42:14 +0200

    finito snack2

--- Snack2/main.js
+++ Snack2/main.js
@@ -2,13 +2,13 @@
 // Generatore di “nomi cognomi” casuali del grande gatsby:
 // Abbiamo una lista di nomi e una lista di cognomi, e da queste vogliamo generare una "falsa" lista di invitati, dove ogni invitato nella lista, è l'unione di un "nome" e un "cognome" delle liste precedentemente dette, scelti in modo casuale.
 
-let nomi = ['marco','luca','paolo']
-let countnomi = nomi.length
+let nomi = ['marco','luca','paolo'];
+let countnomi = nomi.length;
 
-let cognomi = ['ceglie','magistris','vocci']
-let countcognomi = nomi.length
+let cognomi = ['ceglie','magistris','vocci'];
+let countcognomi = nomi.length;
 
-let randomnomi = Math.round(Math.random()*countnomi)+1;
-let randomcognomi = Math.round(Math.random()*countcognomi)+1;
+let randomnomi = nomi[Math.round(Math.random()*(countnomi -1))];
+let randomcognomi = cognomi[Math.round(Math.random()*(countcognomi -1))] ;
 
-console.log(${randomnomi} ${randomcognomi})
+console.log(randomnomi + ' ' + randomcognomi); 
